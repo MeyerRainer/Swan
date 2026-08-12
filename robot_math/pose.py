a11b1d8926db64733360aaa30845b9099f50bfbd
Rainer <r.meyer494@gmail.com>
2026-08-12 14:51:58 +0300

    Modify Pose class.

--- robot_math/pose.py
+++ robot_math/pose.py
@@ -1,4 +1,7 @@
-""" Pose 6D (position + orientation in 3D) """
+""" A Class for 6D Pose (position + orientation in 3D space).
+
+Author: Rainer Meyer, r.meyer494@gmail.com
+"""
 from __future__ import annotations
 
 import utils
@@ -8,90 +11,86 @@ import numpy as np
 
 class Pose:
 
-    def __init__(self, position: np.ndarray, rotation_matrix: np.ndarray):
+    def __init__(self, SE3: np.ndarray = None):
 
-        self._position = position.copy()
-        self._rot_mat = rotation_matrix.copy()
+        self.pose = np.eye(4, dtype=np.float64)
+        if SE3 is not None and SE3.shape == (4,4):
+            self.pose = SE3.copy()
 
     def __repr__(self) -> str:
-        return f"Pose: {self.SE3}"
+        return f"Pose:\n{self.pose}"
 
     def __copy__(self):
-        return Pose(self._position, self._rot_mat)
-
-    def __deepcopy__(self, memo):
-        return Pose(self._position, self._rot_mat)
+        return Pose(self.pose)
 
     def copy(self):
         return self.__copy__()
 
+    # ======================================== Constructors =======================================
+    # TODO: Add orthogonalization, normalization and type checking.
     @classmethod
-    def from_position(cls, pos):
-        return cls(position=pos, rotation_matrix=np.eye(3, dtype=np.float64))
+    def identity(cls) -> Pose:
+        return cls(SE3=np.eye(4, dtype=np.float64))
 
     @classmethod
-    def from_quaternion(cls, pos: np.ndarray, quat: Quaternion):
-        R = quat.to_rotation_matrix()
-        return cls(position=pos, rotation_matrix=R)
+    def from_position(cls, pos: np.ndarray) -> Pose:
+        SE3 = np.eye(4, dtype=np.float64)
+        SE3[:3, 3] = pos
+        return cls(SE3)
 
     @classmethod
-    def from_zyz_euler(cls, pos, zyz):
-        R = utils.zyz2rot_mat(zyz)
-        return cls(position=pos, rotation_matrix=R)
+    def from_quaternion(cls, pos: np.ndarray, quat: Quaternion) -> Pose:
+        SE3 = np.eye(4, dtype=np.float64)
+        SE3[:3, 3] = pos
+        SE3[:3, :3] = quat.to_rotation_matrix()
+        return cls(SE3)
 
     @classmethod
-    def identity(cls):
-        return cls(position=np.zeros(3), rotation_matrix=np.eye(3))
+    def from_zyz_euler(cls, pos: np.ndarray, zyz: np.ndarray) -> Pose:
+        SE3 = np.eye(4, dtype=np.float64)
+        SE3[:3, 3] = pos
+        SE3[:3, :3] = utils.zyz2rot_mat(zyz)
+        return cls(SE3)
 
     @classmethod
-    def from_SE3(cls, T):
-        # TODO: Add orthogonalization and normalization
-        pos = T[:3, 3]
-        R = T[:3, :3]
-        return cls(position=pos, rotation_matrix=R)
+    def from_SE3(cls, T: np.ndarray) -> Pose:
+        return cls(T)
 
+    # ======================================== Getters =======================================
     @property
     def position(self):
-        return self._position.copy()
+        return self.pose[:3, 3]
 
     @property
     def quaternion(self):
-        # return utils.rot_mat2quat(self._rot_mat)
-        return Quaternion.from_rotation_matrix(self._rot_mat)
+        return Quaternion.from_rotation_matrix(self.pose[:3, :3])
 
     @property
     def rot_mat(self):
-        return self._rot_mat.copy()
+        return self.pose[:3, :3]
 
     @property
     def zyz_euler(self):
-        return utils.rot2zyz(self._rot_mat)[0]
+        return utils.rot2zyz(self.pose[:3, :3])[0]
 
     @property
     def SE3(self):
-        T = np.eye(4)
-        T[:3, :3] = self._rot_mat
-        T[:3, 3] = self._position
-        return T
+        return self.pose.copy()
 
+    # ======================================== Setters =======================================
+    # Set position.
     @position.setter
     def position(self, pos):
         if pos.shape != (3,):
             raise ValueError(f"Expected shape (3,), got {pos.shape} instead.")
-        self._position = pos.copy()
+        self.pose[:3, 3] = pos.copy()
 
-    # Set orientation with quaternion
+    # Set orientation using quaternion.
     @quaternion.setter
     def quaternion(self, quat: Quaternion):
-        # if quat.shape != (4,):
-        #     raise ValueError(f"Expected shape (4,), got {quat.shape} instead.")
-        # norm = np.linalg.norm(quat)
-        # if norm < 1e-3:
-        #     raise ValueError("Zero length quaternion")
-        # quat /= norm
-        # self.rot_mat = utils.quat2rot_mat(quat)
-        self._rot_mat = quat.to_rotation_matrix()
+        self.pose[:3, :3] = quat.to_rotation_matrix()
 
+    # Set orientation using rotation matrix.
     @rot_mat.setter
     def rot_mat(self, R: np.ndarray):
         if R.shape != (3, 3):
@@ -104,7 +103,7 @@ class Pose:
             U[:, 2] *= -1
             R = U @ Vt
 
-        self._rot_mat = R
+        self.pose[:3, :3] = R
 
         # # Normalize and orthogonalize using Gram-Schmidt/Cross-Product method
         # # Extract rows (or columns) as vectors
@@ -121,36 +120,85 @@ class Pose:
         # # 3. Calculate the third axis (Z) using a cross product (guarantees det=1)
         # self._rot_mat[2, :] = np.cross(x, y)
 
+    # Set Orientation using ZYZ Euler angles.
     @zyz_euler.setter
     def zyz_euler(self, zyz: np.ndarray):
         if zyz.shape != (3,):
             raise ValueError(f"Expected shape (3,), got {zyz.shape} instead.")
+        self.pose[:3, :3] = utils.zyz2rot_mat(zyz)
 
-        self._rot_mat = utils.zyz2rot_mat(zyz)
-
+    # Set position and orientation using SE3 transformation matrix.
     @SE3.setter
     def SE3(self, T) -> None:
         if T.shape != (4, 4):
             raise ValueError(f"Expected shape (4, 4), got {T.shape} instead.")
-        self.position = T[:3, 3]
-        self.rot_mat = T[:3, :3]
+        self.pose = T.copy()
 
+    # ======================================== Spatial operations =======================================
+    # Rotate around x.
+    def rotate_x(self, angle: float, degrees=False, body_frame=True):
+        R = np.eye(3)
+        if degrees:
+            angle = np.deg2rad(angle)
+        R[1, 1], R[1, 2] = np.cos(angle), -np.sin(angle)
+        R[2, 1], R[2, 2] = np.sin(angle), np.cos(angle)
+        if body_frame:
+            self.pose[:3, :3] = self.pose[:3, :3] @ R
+        else:
+            self.pose[:3, :3] = R @ self.pose[:3, :3]
+
+    # Rotate around y.
+    def rotate_y(self, angle: float, degrees=False, body_frame=True):
+        R = np.eye(3)
+        if degrees:
+            angle = np.deg2rad(angle)
+        R[0, 0], R[0, 2] = np.cos(angle), -np.sin(angle)
+        R[2, 0], R[2, 2] = np.sin(angle), np.cos(angle)
+        if body_frame:
+            self.pose[:3, :3] = self.pose[:3, :3] @ R
+        else:
+            self.pose[:3, :3] = R @ self.pose[:3, :3]
+
+    # Rotate around z.
+    def rotate_z(self, angle: float, degrees=False, body_frame=True):
+        R = np.eye(3)
+        if degrees:
+            angle = np.deg2rad(angle)
+        R[0, 0], R[0, 1] = np.cos(angle), -np.sin(angle)
+        R[1, 0], R[1, 1] = np.sin(angle), np.cos(angle)
+        if body_frame:
+            self.pose[:3, :3] = self.pose[:3, :3] @ R
+        else:
+            self.pose[:3, :3] = R @ self.pose[:3, :3]
+
+    # Return inverse of self.
     def inverse(self) -> Pose:
         inv = Pose.identity()
         inv.rot_mat = self.rot_mat.T
         inv.position = inv.rot_mat @ self.position * -1
         return inv
 
+    # Compose self with the other. For instance, T_A->C = T_A->B * T_B->C.
     def compose(self, other: Pose) -> Pose:
-        return Pose.from_SE3(self.SE3 @ other.SE3)
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
+        return Pose(self.SE3 @ other.SE3)
 
+    # Represents self in another frame. For instance, T_B->C = (T_A->B)^-1 * T_A->C.
     def relative_to(self, other: Pose) -> Pose:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         return other.inverse().compose(self)
 
+    # Euclidean distance from self to the other.
     def distance(self, other: Pose) -> float:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         return np.linalg.norm((self.position - other.position))
 
     def translation_direction(self, other: Pose) -> np.ndarray:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         dir_vec = other.position - self.position
         norm = np.linalg.norm(dir_vec)
         if norm > 1e-6:
@@ -158,16 +206,40 @@ class Pose:
         return np.zeros(3)
 
     def rotation_direction(self, other: Pose) -> np.ndarray:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         return self.quaternion.rotation_direction(other.quaternion)
 
     def angle(self, other: Pose):
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         return self.quaternion.angle(other.quaternion)
 
     def interpolate(self, other: Pose, t: float = 0.5) -> Pose:
         """ Interpolates an intermediate pose between self and other at interpolation parameter t∈[0, 1] """
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         inter_position = self.position + t * (other.position - self.position)
         inter_quaternion = self.quaternion.slerp(other.quaternion, t)
         return Pose.from_quaternion(pos=inter_position, quat=inter_quaternion)
 
-    def is_close(self, other: Pose):
+    def is_close(self, other: Pose) -> bool:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
         return np.allclose(self.SE3, other.SE3, rtol=1e-5)
+
+    # ====================================== Arithmetic ===========================================
+    def __eq__(self, other: Pose) -> bool:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
+        return self.is_close(other)
+
+    def __rmul__(self, other: Pose) -> None:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
+        self.SE3 = self.SE3 @ other.SE3
+
+    def __mul__(self, other: Pose) -> None:
+        if not isinstance(other, Pose):
+            raise TypeError(f"Expected type Pose, got {type(other)} instead.")
+        self.SE3 = other.SE3 @ self.SE3
